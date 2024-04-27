82d937dc096238bc032bf7f0760d86ec2965f73d
Holly Grimm <holly.grimm@gmail.com>
2024-04-27 11:23:53 -0600

    :rocket: update deploy script to deploy two TLDs

--- ignition/modules/SitusTLD.ts
+++ ignition/modules/SitusTLD.ts
@@ -7,10 +7,10 @@ const SitusTLDModule = buildModule("SitusTLDModule", (m) => {
   const situsForbiddenTLDs = m.contract("SitusForbiddenTLDs");
   const situsResolverNonUpgradable = m.contract("SitusResolverNonUpgradable");
 
-  const tldPrice = "900000"; // default price in ETH
+  const tldPrice = "900000"; // TODO: verify TLD Price
 
   const situsTLDFactory = m.contract("SitusTLDFactory", [
-    tldPrice, // FIXME
+    tldPrice,
     situsForbiddenTLDs,
     situsMetadataStore
   ]);
@@ -18,34 +18,52 @@ const SitusTLDModule = buildModule("SitusTLDModule", (m) => {
   m.call(situsForbiddenTLDs, 'addFactoryAddress', [situsTLDFactory]);
   m.call(situsResolverNonUpgradable, 'addFactoryAddress', [situsTLDFactory]);
 
-  const tldName = ".basin";
-  const tldSymbol = ".BASIN";
+  const situsTldName = ".situs";
+  const situsTldSymbol = ".SITUS";
+  const situsTldDomainPrice = 0;
 
-  // Use this for a standard TLD without a custom metadata contract
-  // m.call(situsTLDFactory, 'ownerCreateTld', [
-  //   tldName,
-  //   tldSymbol,
-  //   deployer,
-  //   0,
-  //   false // buying enabled
-  // ]);
+  const basinTldName = ".basin";
+  const basinTldSymbol = ".BASIN";
+  const basinTldDomainPrice = 0;
+
+  // Standard TLD without a custom metadata contract
+  const callNewTLDSitus = m.call(situsTLDFactory, 'ownerCreateTld', [
+    situsTldName,
+    situsTldSymbol,
+    deployer,
+    situsTldDomainPrice,
+    false // buying enabled
+  ]);
+  const situsTLDAddress = m.readEventArgument(callNewTLDSitus, "TldCreated", "tldAddress");
+  const situsTLDContract = m.contractAt("SitusTLD", situsTLDAddress);
+
+  const callNewTLDBasin = m.call(situsTLDFactory, 'ownerCreateTld', [
+    basinTldName,
+    basinTldSymbol,
+    deployer,
+    basinTldDomainPrice,
+    false // buying enabled
+  ],
+  { id: "SitusTLDFactoryOwnerCreateTldBasin"}
+  );
+  const basinTLDAddress = m.readEventArgument(callNewTLDBasin, "TldCreated", "tldAddress", { id: "ReadAddressBasin"});
+  const basinTLDContract = m.contractAt("SitusTLD", basinTLDAddress, { id: "BasinTLD"});
 
   // Use a custom metadata contract
-  const basinMetadata3 = m.contract("BasinMetadata3");
-  const tldRoyalty = "0"; // default price in ETH
-  const basinTLD = m.contract("SitusTLD", [
-    tldName,
-    tldSymbol,
-    deployer,
-    tldPrice, // FIXME
-    true,
-    tldRoyalty,
-    situsTLDFactory,
-    basinMetadata3
-  ]);
+  // const basinMetadata3 = m.contract("BasinMetadata3");
+  // const tldRoyalty = "0"; // default price in ETH
+  // const basinTLD = m.contract("SitusTLD", [
+  //   basinTldName,
+  //   basinTldSymbol,
+  //   deployer,
+  //   basinTldDomainPrice,
+  //   false,
+  //   tldRoyalty,
+  //   situsTLDFactory,
+  //   basinMetadata3
+  // ]);
 
-
-  return { situsMetadataStore, situsForbiddenTLDs, situsResolverNonUpgradable, situsTLDFactory };
+  return { situsMetadataStore, situsForbiddenTLDs, situsResolverNonUpgradable, situsTLDFactory, situsTLDContract, basinTLDContract };
 });
 
 export default SitusTLDModule;
