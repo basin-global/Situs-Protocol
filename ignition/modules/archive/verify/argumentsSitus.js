53eac531dba703ef31279a334c8027398fd22a09
TMO <89156835+tmoindustries@users.noreply.github.com>
2024-05-07 00:01:26 -0600

    Update argumentsSitus.js

--- ignition/modules/archive/verify/argumentsSitus.js
+++ ignition/modules/archive/verify/argumentsSitus.js
@@ -3,8 +3,8 @@ const situsTldSymbol = ".SITUS";
 const deployer = "0x5A4afc30b7D6090DC9911Aa3355979d4C96Cd72E";
 const situsTldDomainPrice = "0";
 const situsRoyalty = "0";
-const factoryAddress = "0x5F9105eB7E5aC3bdB8353835d252C15D4b8e0F6A";
-const metadataAddress = "0x7a6fed2f25623b8C8115F8FAE38aCc0ADA8D0316";
+const factoryAddress = "0x67c814835E1920324634Fd6da416a0E79c949970";
+const metadataAddress = "0x780A9a92BA66a29d942bdb289DB455F8e3ED0dFa";
 
 module.exports = [
   situsTldName,
